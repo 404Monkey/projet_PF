f2e0cb0869c12187681e593397c947a8fb6c7113
Benjamin <cbenjamin174@gmail.com>
2021-03-16 21:56:13 +0100

    parse function

--- projet.ml
+++ projet.ml
@@ -28,12 +28,11 @@
 
 #load "expression_scanner.cmo";;
 open Expression_scanner;;
-
+open List;;
 
 
 (* PARTIE I *)
 (* analyse syntaxique et construction de l'arbre *)
-
 
 (* fonction vérifiant si un mot de Lukasiewicz postfixé est bien formé *)
 let rec is_well_formed_aux token_list =
@@ -57,7 +56,6 @@ let test_well_formed = string_to_token_list "13 2 5 * 1 0 / - + ;";;
 
 is_well_formed test_well_formed;;
 
-
 (* définitions des types pour les arbres *)
 type operator = | Plus | Minus | Mult | Div;;
 type tree =
@@ -67,25 +65,32 @@ type tree =
   | Binary of operator * tree * tree
 ;;
 
-
 (* fonction qui transfome une liste de token en arbre *)
-let rec parse_aux token_list tree stack=
+let rec parse_aux token_list stack=
   match token_list with
-  | [End] -> tree
+  | [] -> hd stack
   | elem::tail -> (
     match elem with
-    | Variable(value) -> value::stack
-    | Number(value) -> value::stack
-    | Minus ->
-    | End -> tree
-    | _ -> 
+    | Variable(value) -> parse_aux tail (Var(value)::stack)
+    | Number(value) -> parse_aux tail (Cst(value)::stack)
+    | Minus -> parse_aux tail (Unary(hd stack)::(tl stack))
+    | Add -> parse_aux tail (Binary(Plus, (hd (tl stack)), (hd stack))::(tl(tl stack)))
+    | Subtract -> parse_aux tail (Binary(Minus, (hd (tl stack)), (hd stack))::(tl(tl stack)))
+    | Multiply -> parse_aux tail (Binary(Mult, (hd (tl stack)), (hd stack))::(tl(tl stack)))
+    | Divide -> parse_aux tail (Binary(Div, (hd (tl stack)), (hd stack))::(tl(tl stack)))
+    | End -> hd stack
+  )
 ;;
+(* trouver un moyen de rassembler tous les opérateurs, le '_' ne marche pas *)
 
 let rec parse token_list =
   if(is_well_formed token_list)
-  then parse_aux(token_list)
+  then parse_aux token_list []
   else failwith "not a Lukasiewicz word" (* mettre autre chose qu'un failwith *)
 ;;
+
+parse test_well_formed;;
+
 
 
 
@@ -101,6 +106,8 @@ let rec simplificate_tree tree =
 
 
 
+
+
 (* PARTIE III *)
 (* Affichage du résultat *)
 
@@ -109,6 +116,8 @@ let rec simplificate_tree tree =
 let display_expr tree =
 
 ;;
+
+
 
 
 
